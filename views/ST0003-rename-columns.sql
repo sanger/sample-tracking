f18605504229304f6ae00d89e189ed2f077d2e86
sabrine33 <sa33@sanger.ac.uk>
2024-05-21 16:38:38 +0100

    rename columns

--- views/ST0003-rename-columns.sql
+++ views/ST0003-rename-columns.sql
@@ -1,8 +1,7 @@
-CREATE OR REPLACE VIEW `${REPORTING_DB}`.`sample_tracking_view` AS
-
+CREATE OR REPLACE VIEW `mlwh_reporting`.`sample_tracking_view` AS
 
 WITH sample_events AS (
-    SELECT event_type, occured_at, subject_uuid_bin FROM `${EVENTS_DB}`.flat_events_view
+    SELECT event_type, occured_at, subject_uuid_bin FROM `mlwh_events`.flat_events_view
     WHERE role_type = 'sample'
       AND event_type IN ('sample_manifest.updated', 'labware.received', 'library_start', 'library_complete', 'sequencing_start', 'sequencing_complete')
       AND occured_at >= DATE_SUB(NOW(), INTERVAL 6 MONTH)
@@ -33,17 +32,17 @@ SELECT
     MAX(IF(sample_events.event_type = 'sequencing_complete', sample_events.occured_at, NULL)) sequencing_qc_complete_last
 
 
-FROM `${REPORTING_DB}`.sample_flowcell_view
+FROM `mlwh_reporting`.sample_flowcell_view
 
          LEFT JOIN sample_events
                    ON sample_events.subject_uuid_bin = sample_flowcell_view.sample_uuid
 
 
-GROUP BY plate_barcode
+GROUP BY manifest_plate_barcode
 -- filter out plates where we have no real information, but leave in rows where there is something for future debugging / smoke testing
-HAVING manifest_created IS NOT NULL
-    OR manifest_submitted IS NOT NULL
+HAVING manifest_uploaded IS NOT NULL
+    OR labware_received IS NOT NULL
     OR library_start_count != 0
     OR library_complete_count != 0
-    OR sequencing_start_count != 0
-    OR sequencing_complete_count !=0;
+    OR sequencing_run_start_count != 0
+    OR sequencing_qc_complete_count !=0;
